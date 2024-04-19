1d8ed207b9421f71fdd5c8374e32e38a14eb40ea
Lina Naranjo <lina.naja1807@gmail.com>
2024-04-18 19:14:52 -0500

    .

--- findy-red-social/src/router/AppRouter.jsx
+++ findy-red-social/src/router/AppRouter.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "../components/Layout/Layout";
+import Layout from "../components/layout/Layout";
 import Home from "../pages/Home/Home";
 import Details from "../pages/Details/Details";
 import Register from "../pages/Register/Register"
